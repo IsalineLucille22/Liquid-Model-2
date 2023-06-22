8137b3c4468d41681484d27937ca8a5906ea7c62
isalinelucilleguex <isalinelucille.guex@gmail.com>
2023-06-22 12:28:14 +0200

    Modification of the name for the threshold in Monod function to be consistent with the text

--- Succinate/Codes/fun_Monod_tot.m
+++ Succinate/Codes/fun_Monod_tot.m
@@ -6,18 +6,18 @@ function dx = fun_Monod_tot(t, z, kappa, threshold)
 %species 1. z(7), dr: biomass resource.
 % Model: S_i + R -> P_i -> 2S_i, P_i <-> S_i + W_i, S_j + W_i -> P_j.
 z(z<=0) = 0;
-T_1 = (z(6) - threshold(1) > 0);
-T_2 = (z(5) - threshold(2) > 0);
+T_W_2 = (z(6) - threshold(1) > 0); %Threshold on W_2 for the used of Pve
+T_W_1 = (z(5) - threshold(2) > 0); %Threshold on W_1 for the used of Ppu
 t_1 = 0; t_2 = 0; %Lag time, if there are lag times then t_i > 0.
-dx_1 = (2*kappa(1,2) + kappa(1,3))*z(3) - kappa(1,1)*z(1)*z(7) - T_1*kappa(1,4)*z(1)*z(6);
-dx_2 = (2*kappa(2,2) + kappa(2,3))*z(4) - kappa(2,1)*z(2)*z(7) - T_2*kappa(2,4)*z(2)*z(5);
-dy_1 = -(kappa(1,2) + kappa(1,3))*z(3) + kappa(1,1)*z(1)*z(7) + T_1*kappa(1,4)*z(1)*z(6);
-dy_2 = -(kappa(2,2) + kappa(2,3))*z(4) + kappa(2,1)*z(2)*z(7) + T_2*kappa(2,4)*z(2)*z(5);
-dw_1 = kappa(1,3)*z(3) - T_2*kappa(2,4)*z(2)*z(5);
-dw_2 = kappa(2,3)*z(4) - T_1*kappa(1,4)*z(1)*z(6);
+dx_1 = (2*kappa(1,2) + kappa(1,3))*z(3) - kappa(1,1)*z(1)*z(7) - T_W_2*kappa(1,4)*z(1)*z(6);
+dx_2 = (2*kappa(2,2) + kappa(2,3))*z(4) - kappa(2,1)*z(2)*z(7) - T_W_1*kappa(2,4)*z(2)*z(5);
+dy_1 = -(kappa(1,2) + kappa(1,3))*z(3) + kappa(1,1)*z(1)*z(7) + T_W_2*kappa(1,4)*z(1)*z(6);
+dy_2 = -(kappa(2,2) + kappa(2,3))*z(4) + kappa(2,1)*z(2)*z(7) + T_W_1*kappa(2,4)*z(2)*z(5);
+dw_1 = kappa(1,3)*z(3) - T_W_1*kappa(2,4)*z(2)*z(5);
+dw_2 = kappa(2,3)*z(4) - T_W_2*kappa(1,4)*z(1)*z(6);
 dr = -(t >= t_1)*kappa(1,1)*z(1)*z(7) - (t >= t_2)*kappa(2,1)*z(2)*z(7);
-du_1 = T_2*kappa(2,4)*z(2)*z(5); %Used by PPU
-du_2 = T_1*kappa(1,4)*z(1)*z(6); %Used by PVE
+du_1 = T_W_1*kappa(2,4)*z(2)*z(5); %Used by PPU
+du_2 = T_W_2*kappa(1,4)*z(1)*z(6); %Used by PVE
 dx = [(t >= t_1)*dx_1; (t >= t_2)*dx_2; (t >= t_1)*dy_1; (t >= t_2)*dy_2; (t >= t_1)*dw_1; (t >= t_2)*dw_2; dr; du_1; du_2; 0];
 
 %% Description
